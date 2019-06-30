54ac5696582fcbae85f33a6945fed7684328b59a
Hai Than <haithan@Hais-MacBook-Pro.local>
2019-06-30 13:37:23 +0700

    hai test

--- src/views/store/NewStoreTicket.vue
+++ src/views/store/NewStoreTicket.vue
@@ -148,7 +148,7 @@ export default {
           c_store_list: this.selectedStore
         }
       }).then(res => {
-        console.log(res.data);
+        console.log(res.data.success);
         this.$router.push("opening-store-tickets");
       }).catch(err => {
         console.log(err);
