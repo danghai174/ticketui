e47a22754c0c96e23594c08aa9ae64e70e5792e0
Apple <apple@Apples-MacBook-Pro.local>
2019-08-16 18:17:27 +0700

    test GA

--- src/views/store/NewStoreTicket.vue
+++ src/views/store/NewStoreTicket.vue
@@ -58,7 +58,7 @@
                 </md-select>
               </md-field>
               <md-field slot="inputs" v-if="selectedDepartment == 'GA'">
-              <vuejs-datepicker></vuejs-datepicker>
+              <datepicker :format="format"></datepicker>
               </md-field>
               <md-field slot="inputs" v-if="selectedDepartment == 'GA'">
                 <div class="flex-column">
@@ -98,7 +98,7 @@
 import { LoginCard, FileUpload } from "@/components";
 import Mixins from "@/plugins/basicMixins";
 import axios from 'axios';
-import Datepicker from 'vuejs-datepicker';
+import * as VueDatepicker from '@livelybone/vue-datepicker';
 
 export default {
   mixins: [Mixins.HeaderImage],
