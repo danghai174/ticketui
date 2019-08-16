2d3b3dd0a3b98a99bb9b5662c9e1c0cb9cbb599d
Apple <apple@Apples-MacBook-Pro.local>
2019-08-16 19:17:00 +0700

    test GA

--- src/views/store/NewStoreTicket.vue
+++ src/views/store/NewStoreTicket.vue
@@ -97,7 +97,7 @@
 import { LoginCard, FileUpload } from "@/components";
 import Mixins from "@/plugins/basicMixins";
 import axios from 'axios';
-import * as Datepicker from '@livelybone/vue-datepicker';
+import Datepicker from '@livelybone/vue-datepicker';
 
 export default {
   mixins: [Mixins.HeaderImage],
